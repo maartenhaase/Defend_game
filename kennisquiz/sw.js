c75d8c53e9ab7522a1739259982dd60aeb1755f9
maartenhaase <info@maartenhaase.com>
2026-09-13 15:40:17 +0200

    Update Wauw Quiz offline assets

--- kennisquiz/sw.js
+++ kennisquiz/sw.js
@@ -1,5 +1,5 @@
 const CACHE='wauw-quiz-v1';
-const ASSETS=["./","./index.html","./style.css","./data.js","./app.js","./manifest.webmanifest","./data/dieren.js","./data/mens.js","./data/aarde.js","./data/ruimte.js","./data/wetenschap.js","./data/geschiedenis.js","./data/taal.js","./data/eten.js","./data/nederland-europa.js","./data/wereld.js"];
+const ASSETS=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest','./data/dieren.js','./data/rest.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
