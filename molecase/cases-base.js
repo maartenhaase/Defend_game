cbfcd15e4b879a81af2dc48df7ccd6f30dc112e9
maartenhaase <info@maartenhaase.com>
2026-09-13 17:16:21 +0200

    Add Mole Case role data

---
+++ molecase/cases-base.js
@@ -0,0 +1,3 @@
+const ROLES={"forensic":{"name":"Forensisch rechercheur","icon":"🧪"},"digital":{"name":"Digitaal rechercheur","icon":"💻"},"tactical":{"name":"Tactisch rechercheur","icon":"👁️"},"financial":{"name":"Financieel rechercheur","icon":"📂"}};
+const ROLE_KEYS=["forensic","digital","tactical","financial"];
+const CASES=[];
