a8a76810641dfcbff23520b115c28b8a03fa34b7
maartenhaase <info@maartenhaase.com>
2026-09-13 16:09:16 +0200

    Bump Wauw Quiz offline cache after full question bank

--- kennisquiz/sw.js
+++ kennisquiz/sw.js
@@ -1,4 +1,4 @@
-const CACHE='wauw-quiz-v1';
+const CACHE='wauw-quiz-v2';
 const ASSETS=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest','./data/dieren.js','./data/rest1.js','./data/rest2.js','./data/rest3.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
