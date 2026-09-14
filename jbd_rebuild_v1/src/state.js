f6347129181921d2443449e0764e18ea81691e5e
maartenhaase <info@maartenhaase.com>
2026-09-14 16:45:24 +0200

    Stage Milestone 6 vehicle state

--- jbd_rebuild_v1/src/state.js
+++ jbd_rebuild_v1/src/state.js
@@ -8,10 +8,11 @@
     bunker:{x:195,y:730,aim:-Math.PI/2,recoil:0,hp:C.bunker.hp,flash:0,muzzle:0,muzzleKind:'mg'},
     artillery:{started:false,t:0,next:0,index:0,finished:false,events:[]},
     assault:{armed:false,t:0,whistle:false,pulse:0,lastReleased:0},
-    enemies:[], bullets:[], explosions:[], fires:[], cover:[], corpses:[],
+    enemies:[], vehicles:[], vehicleShots:[], wrecks:[], bullets:[], explosions:[], fires:[], cover:[], corpses:[],
+    vehicleWave:{started:false,t:0,index:0,finished:false},
     spawn:{t:0,count:0}, particles:[], particleCursor:0,
     trauma:0, quality:1, fps:60, fpsSmoothed:60, levelComplete:false, completeT:0,
-    stats:{shots:0,kills:0,hits:0}, debug:C.debug,
+    stats:{shots:0,kills:0,hits:0,vehicleKills:0,armorHits:0}, debug:C.debug,
     staticCanvas:null, damageCanvas:null, staticCtx:null, damageCtx:null,
     statusText:'TAP DEPLOY', statusSub:'Level 1 · FOOT PATROL',
     ui:{messageT:0,message:''}, audioReady:false
